413433261e205195ba75551b5bea3be10fab154e
kiptalam1 <adamskiptalam0@gmail.com>
2025-05-30 21:12:31 +0300

    add posts route

--- Backend/index.js
+++ Backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import mongoose from "mongoose";
+import postsRoutes from "./routes/posts.route.js";
 
 mongoose
 	.connect(process.env.MONGO_URI)
@@ -15,6 +16,9 @@ app.use(express.json());
 app.get("/", (req, res) => {
 	res.json({ message: "welcome home" });
 });
+
+// routes;
+app.use("/api/posts", postsRoutes);
 
 const PORT = process.env.PORT;
 // listen to app;

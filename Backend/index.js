8045d01527f9c254c29b4d90c414ffa6408796b9
kiptalam1 <adamskiptalam0@gmail.com>
2025-05-30 19:54:48 +0300

    Connect to mongo db

---
+++ Backend/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import mongoose from "mongoose";
+
+mongoose
+	.connect(process.env.MONGO_URI)
+	.then(() => console.log("mongo db connected"))
+	.catch((err) => console.log("MongoDB connection error", err));
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+	res.json({ message: "welcome home" });
+});
+
+const PORT = process.env.PORT;
+// listen to app;
+app.listen(PORT, () => console.log(`it's running !`));

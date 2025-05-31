b148c2f268865680436b73fde8071bd4d99f4cb4
kiptalam1 <adamskiptalam0@gmail.com>
2025-05-31 12:21:05 +0300

    add create post ability

--- Backend/controllers/posts.controller.js
+++ Backend/controllers/posts.controller.js
@@ -1,5 +1,32 @@
 import Post from "../models/post.model.js";
 
+
+export async function createPost(req, res) {
+	const { title, content, published, author } = req.body;
+
+	if (!title || !content || !author)
+		return res.status(400).json({
+			success: false,
+			message: "Title, content, and author are required",
+		});
+
+	try {
+		const post = new Post({ title, content, author, published: !!published });
+
+		await post.save();
+		res.status(201).json({
+			success: true,
+			message: "Post created successfully",
+			data: post,
+		});
+	} catch (error) {
+		res.status(500).json({
+			success: false,
+			message: "Server error",
+		});
+	}
+}
+
 export async function getAllPosts(req, res) {
 	try {
 		const posts = await Post.find();

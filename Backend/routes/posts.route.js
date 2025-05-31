65ab6007945300fb16979cd449322fe001adb284
kiptalam1 <adamskiptalam0@gmail.com>
2025-05-31 19:41:28 +0300

    add routes for updating and deleting a post

--- Backend/routes/posts.route.js
+++ Backend/routes/posts.route.js
@@ -1,12 +1,19 @@
 import express from "express";
 
-import { createPost, getAllPosts } from "../controllers/posts.controller.js";
+import {
+	createPost,
+	getAllPosts,
+	updatePost,
+	deletePost,
+} from "../controllers/posts.controller.js";
 import { authenticateToken } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-// get all posts;
+// post routes;
 router.get("/", getAllPosts);
 router.post("/", authenticateToken, createPost);
+router.put("/:id", authenticateToken, updatePost);
+router.delete("/:id", authenticateToken, deletePost);
 
 export default router;

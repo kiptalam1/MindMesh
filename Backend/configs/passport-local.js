14dc073cbb447e721486f2875c2944884cc25f77
kiptalam1 <adamskiptalam0@gmail.com>
2025-05-31 10:47:27 +0300

    implement passport-local strategy for authentication

---
+++ Backend/configs/passport-local.js
@@ -0,0 +1,37 @@
+import passport from "passport";
+import { Strategy as LocalStrategy } from "passport-local";
+import bcrypt from "bcrypt";
+import User from "../models/user.model";
+
+passport.use(
+	new LocalStrategy(
+		{ usernameField: "email" },
+		async (email, password, done) => {
+			try {
+				const user = await User.findOne({ email });
+				if (!user) return done(null, false, { message: "Email not found" });
+
+				const isPasswordMatch = bcrypt.compare(password, user.password);
+				if (!isPasswordMatch)
+					return done(null, false, { message: "Incorrect password" });
+				// if user is found and password match;
+				return done(null, user);
+			} catch (error) {
+				done(error);
+			}
+		}
+	)
+);
+
+passport.serializeUser((user, done) => {
+	done(null, user._id);
+});
+
+passport.deserializeUser(async (id, done) => {
+	try {
+		const user = await User.findById(id);
+		done(null, user);
+	} catch (error) {
+		done(error);
+	}
+});

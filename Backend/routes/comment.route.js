060260f35a54861da83672d4fabdacc366e0a6bc
kiptalam1 <adamskiptalam0@gmail.com>
2025-05-31 20:23:48 +0300

    add route for create comment

---
+++ Backend/routes/comment.route.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { createComment } from "../controllers/comment.controller.js";
+
+const router = express.Router();
+
+//comments routes;
+router.post("/", createComment);
+
+export default router;

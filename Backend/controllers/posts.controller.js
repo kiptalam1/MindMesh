6254a102cc418dadd5a993ff5df3ff6be2707747
kiptalam1 <adamskiptalam0@gmail.com>
2025-05-31 19:18:33 +0300

    controller to update a post

--- Backend/controllers/posts.controller.js
+++ Backend/controllers/posts.controller.js
@@ -1,6 +1,28 @@
 import Post from "../models/post.model.js";
 
+//update existing post;
+export async function updatePost(req, res) {
+	const updates = req.body;
+	const { id } = req.params;
 
+	try {
+		
+		const findPost = await Post.findByIdAndUpdate(id, updates, { new: true });
+
+		if (!findPost) return res.status(404).json({ success: false, message: "Post not found" });
+
+		return res.status(200).json({success: true, message: "Post updated successfully", data: findPost})
+
+	} catch (error) {
+		res.status(500).json({
+			success: false,
+			message: "Server error",
+		});
+	}
+}
+
+
+// write a new post;
 export async function createPost(req, res) {
 	const { title, content, published, author } = req.body;
 
@@ -27,6 +49,7 @@ export async function createPost(req, res) {
 	}
 }
 
+// retrieve all the posts;
 export async function getAllPosts(req, res) {
 	try {
 		const posts = await Post.find();

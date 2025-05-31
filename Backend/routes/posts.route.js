a66506b71dbfd9dfa91b82bf094872995f401636
kiptalam1 <adamskiptalam0@gmail.com>
2025-05-31 12:18:30 +0300

    add authorization token to create route

--- Backend/routes/posts.route.js
+++ Backend/routes/posts.route.js
@@ -1,10 +1,12 @@
 import express from "express";
 
-import { getAllPosts } from "../controllers/posts.controller.js";
+import { createPost, getAllPosts } from "../controllers/posts.controller.js";
+import { authenticateToken } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 // get all posts;
 router.get("/", getAllPosts);
+router.post("/", authenticateToken, createPost);
 
 export default router;

8368e9f34e5427a00d3990a0efcc6bb5af3cc653
kiptalam1 <adamskiptalam0@gmail.com>
2025-06-03 19:35:14 +0300

    page to display all the post cards

---
+++ Frontend/MindMesh/src/pages/PostsPage.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import PostCard from "../components/PostCard";
+import "../styles/PostsPage.css";
+
+const PostsPage = () => {
+	const [posts, setPosts] = useState([]);
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		const fetchPosts = async () => {
+			try {
+				const results = await fetch("/api/posts");
+				const data = await results.json();
+				setPosts(data.data);
+				setLoading(false);
+			} catch (error) {
+				console.error("Error fetching posts:", error);
+				setLoading(false);
+			}
+		};
+		fetchPosts();
+	}, []);
+
+	return (
+		<div className="posts-page">
+			{loading ? (
+				<div className="loading">loading...</div>
+			) : (
+				<div className="posts-list">
+					{posts.map((post) => (
+						<PostCard key={post._id} post={post} />
+					))}
+				</div>
+			)}
+		</div>
+	);
+};
+
+export default PostsPage;

4be863a452a1d4281b0eec8c917c3bd92fb39a99
kiptalam1 <adamskiptalam0@gmail.com>
2025-06-05 18:49:41 +0300

    add a logout button to the navbar on condition that the user is logged in

--- Frontend/MindMesh/src/components/Navbar.jsx
+++ Frontend/MindMesh/src/components/Navbar.jsx
@@ -1,8 +1,15 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import "../styles/Navbar.css";
+import { useAuth } from "../contexts/AuthContext";
 
 const Navbar = () => {
+	const { isAuthenticated, logout } = useAuth();
+	const navigate = useNavigate();
+
+	const handleLogout = () => {
+		logout();
+		navigate("/");
+	};
 	return (
 		<div className="navbar">
 			<div className="nav-container">
@@ -13,18 +20,28 @@ const Navbar = () => {
 					<NavLink to="/" className="link">
 						Home
 					</NavLink>
-					<NavLink to="/about" className="link">
-						About
-					</NavLink>
 					<NavLink to="/posts" className="link">
 						Posts
 					</NavLink>
-					<NavLink to="/login" className="link">
-						Login
+					<NavLink to="/about" className="link">
+						About
 					</NavLink>
-					<NavLink to="/signup" className="link">
-						Sign Up
-					</NavLink>
+
+					{/* if the user is logged in then show the logout button; */}
+					{isAuthenticated ? (
+						<button onClick={handleLogout} className=" logout-btn">
+							Logout
+						</button>
+					) : (
+						<>
+							<NavLink to="/login" className="link">
+								Login
+							</NavLink>
+							<NavLink to="/signup" className="link">
+								Sign Up
+							</NavLink>
+						</>
+					)}
 				</div>
 			</div>
 		</div>

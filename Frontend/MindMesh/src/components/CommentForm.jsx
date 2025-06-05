7c7a2f3ac481e0415c797862052766e72a3807a2
kiptalam1 <adamskiptalam0@gmail.com>
2025-06-05 21:00:36 +0300

    feat: create a comment form for logged in users

---
+++ Frontend/MindMesh/src/components/CommentForm.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import { useAuth } from "../contexts/AuthContext";
+import "../styles/CommentForm.css";
+
+const CommentForm = ({ postId, onCommentAdded }) => {
+	const [content, setContent] = useState("");
+	const { isAuthenticated, token } = useAuth();
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+
+		// Check if the user is authenticated;
+		if (!isAuthenticated) return;
+
+		try {
+			const response = await fetch("/api/comments", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: `Bearer ${token}`,
+				},
+				body: JSON.stringify({ postId, content }),
+			});
+
+			const data = await response.json();
+
+			if (data.success) {
+				// Reset the comment input field;
+				setContent("");
+				// Call the callback function to notify parent component;
+				onCommentAdded(data.comment);
+			}
+		} catch (error) {
+			console.error("Error submitting comment:", error);
+		}
+	};
+
+	if (!isAuthenticated) {
+		return (
+			<div className="comment-section">
+				<p>Please log in to add a comment.</p>
+			</div>
+		);
+	}
+	return (
+		<form className="comment-form" onSubmit={handleSubmit}>
+			<textarea
+				className="comment-input"
+				placeholder="Write a comment..."
+				required
+				value={content}
+				onChange={(e) => setContent(e.target.value)}
+				name="comment"></textarea>
+			<div>
+				<button type="reset" className="comment-reset">
+					Reset
+				</button>
+				<button type="submit" className="comment-submit">
+					Submit
+				</button>
+			</div>
+		</form>
+	);
+};
+
+export default CommentForm;

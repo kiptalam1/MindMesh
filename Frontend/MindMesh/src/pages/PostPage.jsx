d4ff348caa56a9d9ba4318c190a3907f76ec1a31
kiptalam1 <adamskiptalam0@gmail.com>
2025-06-06 13:22:06 +0300

    feat: add functionality and section to display comments

--- Frontend/MindMesh/src/pages/PostPage.jsx
+++ Frontend/MindMesh/src/pages/PostPage.jsx
@@ -2,10 +2,12 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import "../styles/PostPage.css";
 import CommentForm from "../components/CommentForm";
+import { fetchCommentsByPost } from "../utils/comments.js";
 
 const PostPage = () => {
 	const { postId } = useParams();
 	const [post, setPost] = useState(null);
+	const [comments, setComments] = useState([]);
 	const [loading, setLoading] = useState(true);
 
 	useEffect(() => {
@@ -17,13 +19,26 @@ const PostPage = () => {
 				}
 				const data = await response.json();
 				setPost(data.data);
-				setLoading(false);
 			} catch (error) {
 				console.error("Error fetching post:", error);
+			} finally {
 				setLoading(false);
 			}
 		};
 		fetchPost();
+	}, [postId]);
+
+	useEffect(() => {
+		const fetchComments = async () => {
+			try {
+				const comments = await fetchCommentsByPost(postId);
+				setComments(comments);
+				console.log("comments", comments);
+			} catch (error) {
+				console.error("Error fetching comments", error);
+			}
+		};
+		fetchComments();
 	}, [postId]);
 
 	return (
@@ -43,8 +58,23 @@ const PostPage = () => {
 						})}
 					</span>
 					<p>{post.content}</p>
+					<div className="comments-section">
+						{comments.length === 0 ? (
+							<p>No comments yet</p>
+						) : (
+							comments.map((comment) => (
+								<ul>
+									<li key={comment._id}>
+										<strong>{comment.author?.username || "Anonymous"}:</strong>
+										{comment.content}
+									</li>
+								</ul>
+							))
+						)}
+					</div>
 				</>
 			)}
+
 			{/* comments section */}
 			<CommentForm postId={postId} />
 		</div>

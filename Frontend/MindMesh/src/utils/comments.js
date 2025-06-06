9c624e590e373347be64739656665a217d390b89
kiptalam1 <adamskiptalam0@gmail.com>
2025-06-06 13:20:37 +0300

    fetch comments from the db through the backend

---
+++ Frontend/MindMesh/src/utils/comments.js
@@ -0,0 +1,14 @@
+export const fetchCommentsByPost = async (postId) => {
+	try {
+		const response = await fetch(`api/posts/${postId}/comments`);
+
+		if (!response.ok) {
+			throw new Error("Network response was not ok");
+		}
+		const data = await response.json();
+		return data.data || [];
+	} catch (error) {
+		console.error("Error fetching comments:", error);
+		return [];
+	}
+};

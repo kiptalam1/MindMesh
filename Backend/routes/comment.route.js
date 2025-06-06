7f710cae7b53cc597c03c2b505034b2c1bafe6e9
kiptalam1 <adamskiptalam0@gmail.com>
2025-06-06 17:51:54 +0300

    feat: protect comment routes

--- Backend/routes/comment.route.js
+++ Backend/routes/comment.route.js
@@ -5,13 +5,14 @@ import {
 	getCommentsByPost,
 	updateComment,
 } from "../controllers/comment.controller.js";
+import { authenticateToken } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 //comments routes;
-router.post("/:postId/comments", createComment);
+router.post("/:postId/comments", authenticateToken, createComment);
 router.get("/:postId/comments", getCommentsByPost);
-router.put("/comments/:commentId", updateComment);
-router.delete("/comments/:commentId", deleteComment);
+router.put("/comments/:commentId", authenticateToken, updateComment);
+router.delete("/comments/:commentId", authenticateToken, deleteComment);
 
 export default router;

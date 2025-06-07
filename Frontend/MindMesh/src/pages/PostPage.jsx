e7ab5419cc6bb53b35897db76f7a9a06013a9796
kiptalam1 <adamskiptalam0@gmail.com>
2025-06-07 14:45:40 +0300

    feat: add and style the delete comment button in the comment section

--- Frontend/MindMesh/src/pages/PostPage.jsx
+++ Frontend/MindMesh/src/pages/PostPage.jsx
@@ -1,14 +1,19 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { LuDelete } from "react-icons/lu";
+import { RiDeleteBin6Line, RiDeleteBin6Fill } from "react-icons/ri";
+
 import "../styles/PostPage.css";
 import CommentForm from "../components/CommentForm";
 import { fetchCommentsByPost } from "../utils/comments.js";
+import { useAuth } from "../contexts/AuthContext";
 
 const PostPage = () => {
 	const { postId } = useParams();
 	const [post, setPost] = useState(null);
 	const [comments, setComments] = useState([]);
 	const [loading, setLoading] = useState(true);
+	const { token, user } = useAuth();
 
 	useEffect(() => {
 		const fetchPost = async () => {
@@ -66,10 +71,41 @@ const PostPage = () => {
 							<ul>
 								{comments.map((comment) => (
 									<li key={comment._id}>
-										<strong>
-											~{comment.author?.username || "Anonymous"}:{" "}
-										</strong>
-										{comment.content}
+										<span>
+											<strong>
+												~{comment.author?.username || "Anonymous"}:{" "}
+											</strong>
+											{comment.content}
+										</span>
+										{/* allow author or admin to delete comment */}
+										{(user?.id === comment.author?._id ||
+											user?.role === "admin") && (
+											<button
+												onClick={async () => {
+													try {
+														const res = await fetch(
+															`/api/posts/comments/${comment._id}`,
+															{
+																method: "DELETE",
+																headers: {
+																	Authorization: `Bearer ${token}`,
+																},
+															}
+														);
+														const data = await res.json();
+														if (data.success) {
+															setComments((prev) =>
+																prev.filter((c) => c._id !== comment._id)
+															);
+														}
+													} catch (err) {
+														console.error("Delete failed:", err);
+													}
+												}}
+												className="delete-comment-btn">
+												<RiDeleteBin6Fill />
+											</button>
+										)}
 									</li>
 								))}
 							</ul>

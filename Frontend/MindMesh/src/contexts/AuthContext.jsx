1356cdd8ae46bf5f5064c558aaf13b4e780f4e43
kiptalam1 <adamskiptalam0@gmail.com>
2025-06-07 13:01:19 +0300

    feat: add function to ensure that user is properly logged out after 1 hour

--- Frontend/MindMesh/src/contexts/AuthContext.jsx
+++ Frontend/MindMesh/src/contexts/AuthContext.jsx
@@ -1,4 +1,5 @@
 import { createContext, useContext, useEffect, useState } from "react";
+import { jwtDecode } from "jwt-decode";
 
 const AuthContext = createContext();
 
@@ -6,12 +7,24 @@ export const AuthProvider = ({ children }) => {
 	const [token, setToken] = useState(null);
 	const [isAuthenticated, setIsAuthenticated] = useState(false);
 
+	// check if toke is expired;
+	function isTokenExpired(token) {
+		try {
+			const { exp } = jwtDecode(token);
+			return Date.now() >= exp * 1000; // Convert exp to milliseconds;
+		} catch {
+			return true;
+		}
+	}
+
 	// auto login from localStorage on reload;
 	useEffect(() => {
 		const storedToken = localStorage.getItem("token");
-		if (storedToken) {
+		if (storedToken && !isTokenExpired(storedToken)) {
 			setToken(storedToken);
 			setIsAuthenticated(true);
+		} else {
+			logout(); // remove token if expired or not present;
 		}
 	}, []);
 

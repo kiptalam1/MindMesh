04fac5d600875162f2584e917dbe2370627e782b
kiptalam1 <adamskiptalam0@gmail.com>
2025-06-09 21:26:45 +0300

    styles: add styling to the pages

--- Frontend/Admin/src/App.jsx
+++ Frontend/Admin/src/App.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Homepage from "./pages/Homepage.jsx";
 
 const App = () => {
-	return <div>App</div>;
+	return (
+		<>
+			<Routes>
+				<Route path="/" element={<Homepage />} />
+			</Routes>
+		</>
+	);
 };
 
 export default App;

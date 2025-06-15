f672ca8cd390b16a2f8886e1b1c34ec6ce357459
kiptalam1 <adamskiptalam0@gmail.com>
2025-06-15 15:55:02 +0300

    feat: add functionality to delete comment

---
+++ Frontend/Admin/src/utils/comments.js
@@ -0,0 +1,47 @@
+export async function fetchAllComments() {
+	try {
+		const res = await fetch("/api/comments");
+		if (!res.ok) {
+			throw new Error("Network response was not ok");
+		}
+		const data = await res.json();
+		return {
+			data: data.data || [],
+			message: data.message || "",
+			success: data.success ?? true,
+		};
+	} catch (error) {
+		console.error("Error fetching all comments", error);
+		return {
+			data: [],
+			message: "Failed to fetch comments",
+			success: false,
+		};
+	}
+}
+
+export async function deleteComment(commentId, token) {
+	try {
+		const res = await fetch(`/api/comments/${commentId}`, {
+			method: "DELETE",
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		});
+		if (!res.ok) {
+			throw new Error("Network response was not ok");
+		}
+
+		const data = await res.json();
+		return {
+			success: data.success || "",
+			message: data.message ?? true,
+		};
+	} catch (error) {
+		console.error("Error fetching all comments", error);
+		return {
+			success: false,
+			message: "Failed to delete comment",
+		};
+	}
+}
